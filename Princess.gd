b5d9c572be2db4c189dab9626fadac64297e7a91
Terkwood <metaterkhorn@gmail.com>
2019-11-28 15:16:28 -0500

    Spew randomly

--- Princess.gd
+++ Princess.gd
@@ -6,7 +6,10 @@ extends RigidBody2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	add_force(Vector2(0,0),Vector2(10,10))
+	add_force(
+		Vector2(randi() % 250, randi() % 250),
+		Vector2(randi() % 3333, randi() % 3333)
+	)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
